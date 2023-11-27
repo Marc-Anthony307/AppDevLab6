c9c2f3407eb68221b63b234ead4a79e7ad9fbc3a
Marc-Anthony Iacobellis <2283429@edu.vaniercollege.qc.ca>
2023-11-27 13:44:19 -0500

    Book done

--- LibraryBooks/LibraryBooks/Properties/Settings.Designer.cs
+++ LibraryBooks/LibraryBooks/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace LibraryBooks.Properties {
                 return ((string)(this["BookDatabaseConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\ProductDB.mdf" +
+            ";Integrated Security=True;Connect Timeout=30")]
+        public string ProductDBConnectionString {
+            get {
+                return ((string)(this["ProductDBConnectionString"]));
+            }
+        }
     }
 }
